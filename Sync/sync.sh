04c14c9a250c4b3b806179d7ceb279486dddef79
Ricky Liu <ctrickyliu@mail.scut.edu.cn>
2020-02-26 23:52:59 +0800

    sync.sh增加注释和输入参数

--- Sync/sync.sh
+++ Sync/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# test
+# 本地同步文件脚本(空文件夹不会被同步)
 
 # exec 2>&1-
 # exec >output.log 2>&1 # exec 命令是全脚本有效
@@ -8,16 +8,17 @@ time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "************** ${time} *****************"
 
 function update(){
+	# 根更新文件的函数
 	# echo $1
 	# echo $2
 	if [[ ! -e $2 ]]
 	then
-		`cp $1 $2`
+		`cp $1 $2` # 文件为新添加，直接复制过来
 		echo "${2} has been created."
 	else
 		if [[ $1 -nt $2 ]]
 		then
-			`cp $1 $2`
+			`cp $1 $2` # 文件陈旧，需要更新
 			echo "${2} has been updated."
 		fi
 	fi
@@ -25,20 +26,20 @@ function update(){
 }
 
 function delete(){
-
+	# 由于本脚本没有类似于git的跟踪功能，所以源目标发生删除操作的时候，需要反向检查
 	for element in `ls $2`
 	do
 		src_dir_or_file=$1"/"${element}
 		dest_dir_or_file=$2"/"${element}
 		if [[ -d $dest_dir_or_file ]]
-		then
+		then # 此时 $dest_dir_or_file 为文件夹
 			# if [[ ! -s $dest_dir_or_file ]]
-			if [[ "`ls -A $dest_dir_or_file`" = "" ]]
+			if [[ "`ls -A $dest_dir_or_file`" = "" ]] # 目标文件夹为空，直接删掉
 			then
 				`rm -r $dest_dir_or_file`
 				echo "Dir ${dest_dir_or_file} han been deleted."
-			else
-				delete $src_dir_or_file $dest_dir_or_file
+			else # 否则查看里面的文件在源目标是否还存在
+				delete $src_dir_or_file $dest_dir_or_file 
 			fi
 		else
 			if [[ ! -e $src_dir_or_file ]]
@@ -52,9 +53,9 @@ function delete(){
 
 function getdir(){
 
-	if [[ ! -e $2 ]]
+	if [[ ! -e $2 ]] # 初始化同步文件夹
 	then
-		echo "File not exits, I will create it"
+		echo "File ${2} not exits, I will create it"
 		`mkdir $2`
 	fi
 
@@ -63,11 +64,11 @@ function getdir(){
 		src_dir_or_file=$1"/"${element}
 		dest_dir_or_file=$2"/"${element}
 		if [[ -d $src_dir_or_file ]]
-		then 
-			if [[ ! -e $dest_dir_or_file ]]
-			then
-				`mkdir $dest_dir_or_file`
-			fi
+		then  # 此时 $src_dir_or_file 为文件夹
+			# if [[ ! -e $dest_dir_or_file  ]]
+			# then
+			# 	`mkdir $dest_dir_or_file`
+			# fi
 			getdir $src_dir_or_file $dest_dir_or_file
 		else
 			# $element 是单纯文件名
@@ -82,10 +83,17 @@ function getdir(){
 
 
 
-src_dir='../Algorithms'
-dest_dir='/Users/rickyliu/Documents/Algorithms'
+src_dir=$1
+dest_dir=$2
 
-getdir $src_dir $dest_dir
+if [[ -z $src_dir || -z $dest_dir ]]
+then
+	echo "usage: bash sync.sh src_path dest_path"
+else
+	# echo $src_dir $dest_dir
+	getdir $src_dir $dest_dir
+	echo "Finished!"
+fi
 
 echo -e "********************  E O F  ***********************\n"
 exit 0
