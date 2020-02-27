8e54bed364e93362c2468ccbb3f98d9801d6d6a3
Ricky Liu <ctrickyliu@mail.scut.edu.cn>
2020-02-27 23:54:07 +0800

    更新至新旧比对

--- Sync/syncN.sh
+++ Sync/syncN.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 
 # dir=$1
-dest_dir="."
+dest_dir="./test"
 src_dir="~/test"
 password="XXX"
 host_name="root"
@@ -10,7 +10,13 @@ ip="XXX.XXX.XXX.XXX"
 if [[ ! -e $dest_dir ]]
 then
 	mkdir $dest_dir
-	touch record_old.txt
+fi
+
+cp record.sh ${dest_dir}/record.sh
+cd $dest_dir
+
+if [[ ! -e record_o.txt ]]; then
+	touch record_o.txt
 fi
 
 /usr/bin/expect << EOF # 启用 EXPECT 命令
@@ -55,7 +61,44 @@ EOF
 if [[ ! -e record_o.txt ]]
 then
 	mv record_n.txt record_o.txt
+	while read line
+	do
+		file_o=$line
+		file_o=${file_o/./${src_dir}}
+		read data_o
+		# echo $file_n $data_n
+		/usr/bin/expect << EOF
+		send_user ${file_o}
+EOF
 
+	done < record_o.txt
 fi
 
+while read line
+do
+	file_n=$line
+	read data_n
+	echo $file_n
+	# echo $data_n
+	
+	if grep -q $file_n "record_o.txt"
+	then
+		raw=$(find ./ -name record_o.txt -exec grep -c  $file_n {} \;)
+		echo $raw
+		let "raw=$raw +1 "
+		echo $raw
+		data_o=$(sed -n ${raw}p record_o.txt)
+		if [[ $data_n > $data_o ]]
+		then
+			echo new
+			# update
+		else
+			echo same
+			# do nothing
+		fi
+	else
+		# this is a new file, update
+	fi
+done < record_n.txt
 
+# check whether the old file should be deleted
