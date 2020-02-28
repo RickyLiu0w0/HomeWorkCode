6d46000f18f8261b9fc5a28cf302273a917051c0
Ricky Liu <ctrickyliu@mail.scut.edu.cn>
2020-02-28 14:12:57 +0800

    增加文件创建与更新

--- Sync/syncN.sh
+++ Sync/syncN.sh
@@ -7,26 +7,40 @@ password="XXX"
 host_name="root"
 ip="XXX.XXX.XXX.XXX"
 
+function mkdirHelp(){
+	local dir=$1 # bash 变量默认全局变量
+	if [[ ! -e $dir ]]
+	then
+		mkdirHelp ${dir%/*}
+		echo "mkdir $dir"
+		mkdir $dir
+	else
+		echo "$dir exists"
+	fi
+}
+
 if [[ ! -e $dest_dir ]]
 then
 	mkdir $dest_dir
 fi
 
-cp record.sh ${dest_dir}/record.sh
+cp record.sh ${dest_dir}/.record.sh
+cp scpHelp.exp ${dest_dir}/.scpHelp.exp
 cd $dest_dir
 
-if [[ ! -e record_o.txt ]]; then
-	touch record_o.txt
+if [[ ! -e record_o.txt ]]
+then
+	touch .record_o.txt
 fi
 
 /usr/bin/expect << EOF # 启用 EXPECT 命令
 set timeout 5
-spawn scp record.sh ${host_name}@${ip}:${src_dir}/record.sh
+spawn scp .record.sh ${host_name}@${ip}:${src_dir}/.record.sh
 	expect {
 		"(yes/no)*" { send "yes\r"; exp_continue}
 		"password" { send "${password}\r"; exp_continue}
 		"No such file or directory" { send_user "error\n"; exit 1}
-		 timeout { send_user "expect was timeout\n"; return }
+		 timeout { send_user "expect was timeout\n"; exit }
 
 	}
 	# expect eof
@@ -40,7 +54,7 @@ spawn ssh -l ${host_name} ${ip}
 
 	expect  {
 		"~#" { send "cd ${src_dir}\r"; exp_continue}
-		"*${src_dir}#" { send "bash record.sh \r"}
+		"*${src_dir}#" { send "bash .record.sh \r"}
 	}
 
 	expect  {
@@ -48,7 +62,7 @@ spawn ssh -l ${host_name} ${ip}
 		timeout { send_user "error\n"; exit 2}
 	}
 
-spawn scp ${host_name}@${ip}:${src_dir}/record.txt record_n.txt
+spawn scp ${host_name}@${ip}:${src_dir}/.record.txt .record_n.txt
 	expect {
 		"(yes/no)*" { send "yes\r"; exp_continue}
 		"password" { send "${password}\r"; exp_continue}
@@ -56,49 +70,56 @@ spawn scp ${host_name}@${ip}:${src_dir}/record.txt record_n.txt
 		 timeout { send_user "expect was timeout\n"; exit 2 }
 	}
 	# expect eof
+	exit 0
 EOF
 
-if [[ ! -e record_o.txt ]]
-then
-	mv record_n.txt record_o.txt
-	while read line
-	do
-		file_o=$line
-		file_o=${file_o/./${src_dir}}
-		read data_o
-		# echo $file_n $data_n
-		/usr/bin/expect << EOF
-		send_user ${file_o}
-EOF
+# if [[ ! -e .record_o.txt ]]
+# then
+# 	mv .record_n.txt .record_o.txt
+# 	while read line
+# 	do
+# 		file_o=$line
+# 		file_o=${file_o/./${src_dir}}
+# 		read data_o
+# 		# echo $file_n $data_n
+# 		/usr/bin/expect << EOF
+# 		send_user ${file_o}
+# EOF
 
-	done < record_o.txt
-fi
+# # 	done < record_o.txt
+# # fi
 
 while read line
 do
 	file_n=$line
 	read data_n
-	echo $file_n
+	# echo $file_n
 	# echo $data_n
 	
-	if grep -q $file_n "record_o.txt"
+	if grep -q $file_n ".record_o.txt"
 	then
-		raw=$(find ./ -name record_o.txt -exec grep -c  $file_n {} \;)
-		echo $raw
+		raw=$(find ./ -name .record_o.txt -exec grep -c  $file_n {} \;)
+		# echo $raw
 		let "raw=$raw +1 "
-		echo $raw
-		data_o=$(sed -n ${raw}p record_o.txt)
+		# echo $raw
+		data_o=$(sed -n ${raw}p .record_o.txt)
+		# file_n=${file_n/./${src_dir}}
+
 		if [[ $data_n > $data_o ]]
 		then
-			echo new
 			# update
+			echo "${file_n} needs to be updated."
+			# expect .scpHelp.exp ${host_name}"@"${ip} $password ${file_n/./${src_dir}} $file_n
 		else
-			echo same
 			# do nothing
+			echo "No change with ${file_n}."
 		fi
 	else
-		# this is a new file, update
+		# download
+		echo "${file_n} is new."
+		mkdirHelp ${file_n%/*}
+		expect .scpHelp.exp ${host_name}"@"${ip} $password ${file_n/./${src_dir}} $file_n
 	fi
-done < record_n.txt
+done < .record_n.txt
 
 # check whether the old file should be deleted
